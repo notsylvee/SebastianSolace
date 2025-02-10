fa6bdc3ef599092760cb00b5c7d4968469e5d3e9
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-02-09 22:27:52 -0500

    changed the command name

--- src/commands/commands/friend.js
+++ src/commands/commands/friend.js
@@ -2,10 +2,10 @@ const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("invite")
-    .setDescription(" ")
+    .setName("imaginary")
+    .setDescription("Get an invite for the Imaginary Friend bot")
     .addSubcommand((command) =>
-    command.setName("ImaginaryFriend").setDescription("Get an invite for the Imaginary Friend bot")),
+    command.setName("friend").setDescription("Get an invite for the Imaginary Friend bot")),
 
     async execute(interaction) {
       interaction.reply({ content: "https://discord.com/oauth2/authorize?client_id=1318490033464021032", ephemeral: true });
