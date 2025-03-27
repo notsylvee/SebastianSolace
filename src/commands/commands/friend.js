4e1e5fbd49c3a7a8d98f50586d1caf981e3cbec7
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-03-26 22:28:42 -0400

    changed "ephemeral" to a flag

--- src/commands/commands/friend.js
+++ src/commands/commands/friend.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder, MessageFlags } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -8,6 +8,6 @@ module.exports = {
     command.setName("friend").setDescription("Get an invite for the Imaginary Friend bot")),
 
     async execute(interaction) {
-      interaction.reply({ content: "https://discord.com/oauth2/authorize?client_id=1318490033464021032", ephemeral: true });
+      interaction.reply({ content: "https://discord.com/oauth2/authorize?client_id=1318490033464021032", flags: MessageFlags.Ephemeral });
     },
 };
