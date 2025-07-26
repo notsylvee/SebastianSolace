a3ad1c1f53bf230fe366df71e9051bb0e2d5a575
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-07-25 21:26:35 -0400

    added /scanner

---
+++ src/commands/commands/scanner.js
@@ -0,0 +1,18 @@
+const fs = require("fs/promises");
+
+module.exports = {
+    data: {
+        name: "scanner",
+        description: "Sends a random Sebastian's Scanner voiceline",
+        "integration_types": [0, 1],
+        "contexts": [0, 1, 2]
+    },
+    async execute(interaction) {
+        const voicelinesJsonData = await fs.readFile("data/voicelines.json", {encoding: "utf8"});
+        const voicelinesMap = JSON.parse(voicelinesJsonData);
+        const voicelines = voicelinesMap["scanner"];
+        const voiceline = voicelines[Math.floor(Math.random() * voicelines.length)];
+            
+        interaction.reply({ content: `<:SebastiansScanner:1398476449417134150> ${voiceline.text}` });
+      },
+}
